2b685b7b3f09271dfd59cc2581dd894f2ecf0e18
Chris Drymon <chrisdrymon@yahoo.com>
2018-04-02 13:41:19 -0500

    A file of utility scripts that will be frequently imported into other scripts. Currently, the only one is the "deaccent" script which removes accents, diacritical marks, text critical marks, and anything ASCII (for my purposes, English) from a given string.

---
+++ Utility.py
@@ -0,0 +1,27 @@
+import string
+
+def deaccent(dastring):
+    """Returns an unaccented version of dastring."""
+    aeinput = "άἀἁἂἃἄἅἆἇὰάᾀᾁᾂᾃᾄᾅᾆᾇᾰᾱᾲᾳᾴᾶᾷἈἉΆἊἋἌἍἎἏᾈᾉᾊᾋᾌᾍᾎᾏᾸᾹᾺΆᾼέἐἑἒἓἔἕὲέἘἙἚἛἜἝΈῈΈ"
+    aeoutput = "ααααααααααααααααααααααααααΑΑΑΑΑΑΑΑΑΑΑΑΑΑΑΑΑΑΑΑΑΑεεεεεεεεεΕΕΕΕΕΕΕΕΕ"
+    hoinput = "ΉῊΉῌἨἩἪἫἬἭἮἯᾘᾙᾚᾛᾜᾝᾞᾟήἠἡἢἣἤἥἦἧὴήᾐᾑᾒᾓᾔᾕᾖᾗῂῃῄῆῇὀὁὂὃὄὅόὸόΌὈὉὊὋὌὍῸΌ"
+    hooutput = "ΗΗΗΗΗΗΗΗΗΗΗΗΗΗΗΗΗΗΗΗηηηηηηηηηηηηηηηηηηηηηηηηοοοοοοοοοΟΟΟΟΟΟΟΟΟ"
+    iuinput = "ΊῘῙῚΊἸἹἺἻἼἽἾἿΪϊίἰἱἲἳἴἵἶἷΐὶίῐῑῒΐῖῗΫΎὙὛὝὟϓϔῨῩῪΎὐὑὒὓὔὕὖὗΰϋύὺύῠῡῢΰῦῧ"
+    iuoutput = "ΙΙΙΙΙΙΙΙΙΙΙΙΙΙιιιιιιιιιιιιιιιιιιιΥΥΥΥΥΥΥΥΥΥΥΥυυυυυυυυυυυυυυυυυυυ"
+    wrinput = "ώὠὡὢὣὤὥὦὧὼώᾠᾡᾢᾣᾤᾥᾦᾧῲῳῴῶῷΏὨὩὪὫὬὭὮὯᾨᾩᾪᾫᾬᾭᾮᾯῺΏῼῤῥῬ"
+    wroutput = "ωωωωωωωωωωωωωωωωωωωωωωωωΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩΩρρΡ"
+    # Strings to feed into translator tables to remove diacritics.
+
+    aelphas = str.maketrans(aeinput, aeoutput, "⸀⸁⸂⸃·,.—")
+    # This table also removes text critical markers and punctuation.
+
+    hoes = str.maketrans(hoinput, hooutput, string.punctuation)
+    # Removes other punctuation in case I forgot any.
+
+    ius = str.maketrans(iuinput, iuoutput, '0123456789')
+    # Also removes numbers (from verses).
+
+    wros = str.maketrans(wrinput, wroutput, string.ascii_letters)
+    # Also removes books names.
+
+    return dastring.translate(aelphas).translate(hoes).translate(ius).translate(wros).lower()
